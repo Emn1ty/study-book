b816e8b6a4eee6a5d1f9797593647e1b8860e2f3
emn1ty <jon.ardis@gmail.com>
2013-10-26 22:57:44 -0700

    added stream transform and doubler class

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-"use strict";
-
 // require stream
 var stream = require('stream');
 
-// create input stream
-var stdin = process.openStdin();
-
-// create transform stream
-var uppercase = new stream.Transform({decodeStrings: false});
-
-uppercase._transform = function (chunk, encoding, done) {
-  done(null, chunk.toString().toUpperCase());
+// uppercaser stream
+var Uppercaser = function Uppercaser() {
+  stream.Transform.call(this);
 };
 
-stdin.on('data', function (chunk) {
-  
-  // no idea what to do next
-  this.pipe(uppercase);
-});
+// inherit from stream.Transform class and reassing name to original class
+Uppercaser.prototype = Object.create(stream.Transform.prototype, {constructor: {value: Uppercaser}});
+
+// create transform definition
+Uppercaser.prototype._transform = function _transform(chunk, encoding, done) {
+  this.push(chunk.toString().toUpperCase());
+  done();
+};
+
+// uppercaser stream
+var Doubler = function Doubler() {
+  stream.Transform.call(this);
+};
+
+// inherit from stream.Transform class and reassing name to original class
+Doubler.prototype = Object.create(stream.Transform.prototype, {constructor: {value: Uppercaser}});
+
+// create transform definition
+Doubler.prototype._transform = function _transform(chunk, encoding, done) {
+  this.push(chunk.toString());
+  this.push(chunk.toString());
+  done();
+};
+
+
+// connect stream
+process.stdin.pipe(new Uppercaser()).pipe(new Buffer()).pipe(process.stdout);
