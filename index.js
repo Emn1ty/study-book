fc9e66c01ed1cca1d61e41975ba43dd7bc7d7dee
emn1ty <jon.ardis@gmail.com>
2013-10-26 19:53:38 -0700

    first attempt at adding stream.Transform

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 "use strict";
 
+// require stream
 var stream = require('stream');
 
+// create input stream
 var stdin = process.openStdin();
-var transform = stream.Transform();
 
-stdin.on('data', function(chunk) {
-	process.stdout.write(chunk.toString().toUpperCase());
+// create transform stream
+var uppercase = new stream.Transform({decodeStrings: false});
+
+uppercase._transform = function (chunk, encoding, done) {
+  done(null, chunk.toString().toUpperCase());
+};
+
+stdin.on('data', function (chunk) {
+  this.pipe(uppercase);
 });
